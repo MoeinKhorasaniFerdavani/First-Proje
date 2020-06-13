6ec3fd966f3e0152e5551d6d366f8651eda867d1
MoeinKhorsaniFerdavani <moeinferdavani1380@gmail.com>
2020-06-14 01:28:04 +0430

    make Date class

--- date.cpp
+++ date.cpp
@@ -1,6 +1,79 @@
 #include "date.h"
 
-Date::Date()
+void Date:: setYear(int year)
 {
+    if(year<=0)
+        return;
+   else if(year<100)
+    {
+        year+=2000;
+        this->year=year;
+        return;
+    }
+    else
+    {
+        this->year=year;
+        return;
+    }
+}
+
+
+void Date::setMonth(int month)
+{
+    if(month<12 && month>0)
+        this->month=month;
+    return;
+}
+
+void Date:: setDay(int day)
+{
+    if(day>0)
+        this->day=day;
+    return;
+}
+
+void Date:: setHour(int hour)
+{
+    if(hour<24&& hour>=0)
+        this->hour=hour;
+    return;
+}
+
+void Date::setMinute(int minute)
+{
+    if(minute>=0 && minute<60)
+        this->minute=minute;
+}
+
+
+Date::Date(int year,int month,int day,int hour,int minute)
+
+{
+   this->setYear(year);
+    this->setMonth(month);
+     this->setDay(day);
+     this->setHour(hour);
+     this->setMinute(minute);
 
 }
+
+void Date:: goToCurrentDate()
+{
+    this->year=localtime(& now)->tm_year;
+     this->month=localtime(& now)->tm_mon;
+     this->day=localtime(& now)->tm_mday;
+     this->hour=localtime(& now)->tm_hour;
+     this->minute=localtime(& now)->tm_min;
+
+}
+
+const char* Date:: toString()
+{
+    char date [100];
+    sprintf(date,"date:\t%d/%d/%d\ttime:\t%d/%d",this->year,this->month,this->day,  this->hour,this->minute);
+
+    return date;
+
+}
+
+
--- date.h
+++ date.h
@@ -1,11 +1,46 @@
 #ifndef DATE_H
 #define DATE_H
 
+#include <time.h>
+#include <iostream>
+#include <string>
+
+using namespace  std;
 
 class Date
 {
+
+    int year=2020;
+    int month=1;
+    int day=1;
+    int hour=0;
+    int minute=0;
+   time_t now=0;
 public:
-    Date();
+
+
+   int getYear()const{return this->year;}
+   int getMonth()const{return this->month;}
+   int getDay()const{return this->day;}
+   int getHour()const{return this->hour;}
+   int getMinute()const{return this->minute;}
+
+
+   void setYear(int year);
+ void setMonth(int month);
+ void setDay(int day);
+ void setHour(int hour);
+ void setMinute(int minute);
+
+
+    Date(int year=2020,int month=1,int day=1,int hour=0,int minute=0);
+     void goToCurrentDate();
+
+
+   const char* toString();
+
+
+
 };
 
 #endif // DATE_H
