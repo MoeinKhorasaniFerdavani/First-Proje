86825affcb4f013504e0b0dddd77f14883f68ff2
MoeinKhorsaniFerdavani <moeinferdavani1380@gmail.com>
2020-06-13 11:35:45 +0430

    make Node class

---
+++ Node.h
@@ -0,0 +1,68 @@
+#ifndef NODE_H
+#define NODE_H
+
+
+
+
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class Node
+{
+    T data;
+    T* prev, * next;
+public:
+    Node(const T& data)
+        :prev(nullptr),next(nullptr)
+    {
+        //Conversion Constructor
+        this->data = data;
+    }
+
+    Node (const Node<T> & rvalue)
+    {
+       throw error_code();
+    }
+
+    void setData(const T& data) { this->data = data; }
+    void setPrev(T* ptr) { this->prev = ptr; }
+    void setNext(T* ptr) { this->next = ptr; }
+
+    T  getData()const { return this->data; }
+    T* getPrev()const { return this->prev; }
+    T* getNext()const { return this->next; }
+
+    T operator= (const T& data)
+    {
+        this->data = data;
+        return data;
+    }
+
+
+
+};
+
+
+template <typename T>
+ostream& operator<< (ostream& out, const Node<T>& node)
+{
+    //Print Function
+    cout << node.getData();
+    return out;
+}
+template <typename T>
+void swapContent(Node<T> & a, Node<T>& b)
+{
+    T temp = a.getData();
+    a.setData(b.getData());
+    b.setData(temp);
+
+    return;
+}
+
+
+
+
+#endif // NODE_H
